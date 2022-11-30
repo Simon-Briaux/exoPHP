ea2d0be9b5a21a044b382eea91f063c433ad389a
Simon <simon.briaux2@gmail.com>
2022-11-30 15:13:49 +0100

    fin exo

---
+++ exo1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo1.php</title>
+</head>
+<body>
+    <div>
+        <?php
+            $aléatoire = rand (0,100);
+            if ($aléatoire %2 == 0){
+                echo"<p style='background-color:red;'>votre nombre $aléatoire est pair</p>";
+            }  
+            else{
+                echo"<p style='background-color:blue;'>votres nombre $aléatoire est impair";
+            }
+        ?>
+    </div>
+</body>
+</html>
