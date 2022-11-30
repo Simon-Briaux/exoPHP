b3b4d9b3d78c23a3f0f552e90c77cc0939bb77cb
Simon <simon.briaux2@gmail.com>
2022-11-30 15:49:05 +0100

    fin exo

---
+++ exo2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo2</title>
+</head>
+<body>
+<?php
+
+$array = [
+    0 => 1,
+    1 => 2,
+    2 => 3,
+    3 => 4,
+    4 => 5,
+];
+echo "<table border='1'><tr>";
+
+for($i=0;$i<5;$i++){
+    echo "<th>".$array[$i]."</th>";
+}
+echo "</tr></table>";
+?>    
+
+<table>
+<tr>
+
+
+</tr>
+</table>
+
+</body>
+</html>
