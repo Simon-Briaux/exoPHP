bf6aff1dd1f8e4f7972a13bdc85bca098d9e68ab
Simon <simon.briaux2@gmail.com>
2022-11-30 17:15:19 +0100

    css

--- exo2.php
+++ exo2.php
@@ -4,25 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="exo2.css" />
     <title>exo2</title>
 </head>
 <body>
-<?php
+    <div>
+        <?php
+        $array = [
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 4,
+            4 => 5,
+        ];
+        echo "<table border='1'><tr>";
 
-$array = [
-    0 => 1,
-    1 => 2,
-    2 => 3,
-    3 => 4,
-    4 => 5,
-];
-echo "<table border='1'><tr>";
+        for($i=0;$i<5;$i++){
+            echo "<th>".$array[$i]."</th>";
+        }
+        echo "</tr></table>";
+        ?>   
+    </div>
+    <div> 
+        <?php
+        $array = [
+            0 => "Nom",
+            1 => "Prenom",
+            2 => "MDP",
+        ];
+        echo "<table border='1'><tr>";
 
-for($i=0;$i<5;$i++){
-    echo "<th>".$array[$i]."</th>";
-}
-echo "</tr></table>";
-?>    
+        for($i=0;$i<3;$i++){
+            echo "<th>".$array[$i]."</th>";
+        }
+        echo "</tr></table>";
+        ?>
+    </div>
 
 <table>
 <tr>
