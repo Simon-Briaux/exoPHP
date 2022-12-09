794e1940663e4745f482819a69adfe73fd54bf7e
Simon <simon.briaux2@gmail.com>
2022-12-09 16:13:00 +0100

    changement de nom css

--- exo2.php
+++ exo2.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="exo2.css" />
+    <link rel="stylesheet" href="php.css" />
     <title>exo2</title>
 </head>
 <body>
