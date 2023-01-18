c993a567c5a2f6148c7b8c73dc45fdfdbb37e762
Simon <simon.briaux2@gmail.com>
2023-01-18 15:14:08 +0100

    essayer de reparer

--- exo1.php
+++ exo1.php
@@ -1,31 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>exo1.php</title>
-=======
-    <title>exo1</title>
->>>>>>> 3190eafc32909f47274a88dae4a98f927639539d
-</head>
-<body>
-    <div>
-        <?php
-<<<<<<< HEAD
-            $aléatoire = rand (0;100);
-            echo"le nombre aléatoire est .$aléatoire."; 
-=======
-            $aléatoire = rand (0,100);
-            if ($aléatoire %2 == 0){
-                echo"<p style='background-color:red;'>votre nombre $aléatoire est pair</p>";
-            }  
-            else{
-                echo"<p style='background-color:blue;'>votres nombre $aléatoire est impair";
-            }
->>>>>>> 3190eafc32909f47274a88dae4a98f927639539d
-        ?>
-    </div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>exo1</title>
+    </head>
+    <body>
+        <div>
+            <?php
+                $aléatoire = rand (0,100);
+                echo"le nombre aléatoire est .$aléatoire."; 
+                $aléatoire = rand (0,100);
+                if ($aléatoire %2 == 0){
+                    echo"<p style='background-color:red;'>votre nombre $aléatoire est pair</p>";
+                }  
+                else{
+                    echo"<p style='background-color:blue;'>votres nombre $aléatoire est impair";
+                }
+            ?>
+        </div>
+    </body>
 </html>
